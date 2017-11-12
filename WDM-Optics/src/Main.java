895a0fcc15022315aa38d6c51f83a89198194046
Eleni T <trachanid@csd.auth.gr>
2017-11-13 00:57:29 +0200

    First code sample

---
+++ WDM-Optics/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Calendar;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Station station =new Station();
+		
+		
+		
+//		station.station("PC1",1,'a');
+//		station.station("PC2",2,'a');
+//		station.station("PC3",3,'b');
+//		station.station("PC4",4,'b');
+//		station.station("PC5",5,'c');
+//		station.station("PC6",6,'c');
+//		station.station("PC7",7,'d');
+//		station.station("PC8",8,'d');
+		
+		for(int j=0;j<5;j++){
+		station.PC();}
+		//station.iterateTable();
+		//System.out.println("number is:"+ );
+		}
+	}
+
+
---
+++ WDM-Optics/src/Queue.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Queue {
+	
+	
+	 int i=0;
+	PriorityQueue<Integer> q= new PriorityQueue<Integer>();
+	Packet p= new Packet();
+	
+	public Boolean empty(){
+		return q.isEmpty();
+	}
+	
+	//Put packet in queue
+	public void add(Packet packet) {					
+		
+			//Add on list
+		//
+		
+		q.offer(p.getId());
+		
+	//}i++;
+		//System.out.println(q);
+	}
+	
+	
+	//Remove element with the highest priority from list
+	public void remove() {	
+		//while(!q.isEmpty()) || i<5)
+				//{
+			
+				q.poll();
+			//}
+		//i++;
+		
+		
+		//System.out.println(q);						
+	}
+	
+	
+	
+}
