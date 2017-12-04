8bc8a41bc14f9ac49adb689a8a53abeaa5b3d6ec
Eleni T <trachanid@csd.auth.gr>
2017-12-04 14:37:39 +0200

    2_2

---
+++ WDM-Optics/src2_2/pck2/Main.java
@@ -0,0 +1,14 @@
+package pck2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 PC pc1= new PC();
+		 for(int f=0;f<10;f++) {
+		 pc1.arrive_broadcast_In_Queue();
+		 }
+	}
+
+}
