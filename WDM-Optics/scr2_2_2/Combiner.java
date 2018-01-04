36a43708880db5b2941c11f167c5dba95aa466e0
Eleni T <trachanid@csd.auth.gr>
2018-01-04 15:05:51 +0200

    Mae some changes.For now working

---
+++ WDM-Optics/scr2_2_2/Combiner.java
@@ -0,0 +1,28 @@
+
+public class Combiner {
+		//elegxos sugkrousewn packetwn
+	//list pou 8a sugkrinontai 2 paketa th fora(an idia 0 an diaforetika 1)
+	
+//	public void put_in_list(PC pc) {
+//		//edw mpainei to broadcast to PC
+//	}
+	//sugkrish 1 kai 2 keliwn
+	public void compare_packet_list(PC pc1, PC pc2 , int key) {
+		if(pc1.map_value(key)==pc2.map_value(key)) {
+			System.out.println("PC1:"+pc1.map_value(key));
+			pc1.map_remove(key);
+			pc2.map_remove(key);
+			System.out.println("There's been a collision");
+			System.out.println("PC1:"+pc1.map_value(key));
+		}
+		else {
+			System.out.println("continue");
+		}
+	
+
+	}
+	
+	
+	
+	
+}
---
+++ WDM-Optics/scr2_2_2/Main.java
@@ -0,0 +1,85 @@
+import java.util.Random;
+
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		 PC1 pc1= new PC1();
+//		 PC pc= new PC();
+//		 double probability;
+//		for(int f=0;f<10;f++) {
+//		
+//		 pc1.arrive_broadcast_In_Queue(f);
+//		 }
+//	}
+//
+//}
+
+
+public class Main {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double probability;
+		int f=0;
+		random rand=new random();
+		
+		
+		 PC pc1= new PC();
+		 PC pc2= new PC();
+		 Long average_Delay;
+		Combiner combine= new Combiner();
+		 //probability=rand.random();
+		 
+		 //13packet sent
+		
+		 for(f=0;f<13;f++) {
+			 probability=rand.random();
+				//System.out.println("Probability is: "+probability);
+			 if(probability>0.3) {
+			
+		 pc1.arrive_broadcast_In_Queue(probability);//,f);
+		// pc2.arrive_broadcast_In_Queue(probability);//,f);
+		//probability=random();
+		 }
+		 }	
+		
+	
+		 }
+		 
+		
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+//		 for(int j=0;j<(pc1.map_size()+1);j++) {
+//			 if(pc1.map_size()==0 && pc2.map_size()==0) {
+//				 System.out.println("Empty map");
+//			 }
+//			 if(pc1.map_size()==0 && pc2.map_size()!=0) {
+//				 System.out.println("Empty is  map1 not 2.Map 2 is:"+ pc2.map_size());
+//				 
+//			 }
+//			 if(pc1.map_size()!=0 && pc2.map_size()==0) {
+//				 System.out.println("Empty is map2 not 1.Map 1 is:"+ pc1.map_size());
+//				
+//			 }
+//			 if(pc1.map_size()!=0 && pc2.map_size()!=0) {
+//			 combine.compare_packet_list(pc1, pc2 ,j);
+//			 }
+//		 }
+		 //}
+//		 System.out.println("So~");
+		 
+		
+		 
+	}
+
+
